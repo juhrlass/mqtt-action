e0d09629b3230941a0247889a9d0ffe49cbdf0a5
Joachim Uhrlaß <ecapot@gmail.com>
2020-07-07 22:08:10 +0200

    Updates

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const mqtt = require('mqtt')
 const core = require('@actions/core')
 
-const client = mqtt.connect({
+const options = {
   protocol: core.getInput('protocol'),
   host: core.getInput('host'),
   port: core.getInput('port'),
   username: core.getInput('username'),
   password: core.getInput('password')
-})
+}
+
+const topic = core.getInput('topic')
+const message = core.getInput('message')
+
+console.log('Try connecting to mqtt broker')
+
+const client = mqtt.connect(options)
 
 client.on('connect', function () {
-  client.publish(core.getInput('topic'), core.getInput('message'), function (err) {
+  console.log('Connected!')
+  client.publish(topic, message, function (err) {
     if (!err) {
       console.log('Successfully published message to topic')
     } else {
